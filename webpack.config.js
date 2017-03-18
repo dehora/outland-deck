5e502e4c533060e49eb63c026d46c0d7d2b28dec
dehora <bill@dehora.net>
2017-03-18 23:36:45 +0000

    Adds react routing and react toolbox.

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,92 @@
-'use strict';
+const path = require('path');
+const webpack = require('webpack');
 
-var webpack = require('webpack'),
-    jsPath  = 'app/assets/javascripts',
-    path = require('path'),
+var jsPath  = 'app/assets/javascripts',
     srcPath = path.join(__dirname, 'app/assets/javascripts');
 
-var config = {
-    target: 'web',
-    entry: {
-        app: path.join(srcPath, 'app.jsx')
-        //, common: ['react-dom', 'react']
-    },
-    resolve: {
-        alias: {},
-        root: srcPath,
-        extensions: ['', '.js'],
-        modulesDirectories: ['node_modules', jsPath]
-    },
-    output: {
-        path:path.resolve(__dirname, jsPath, 'build'),
-        publicPath: '',
-        filename: '[name].js',
-        pathInfo: true
-    },
+const settings = {
 
-    module: {
-        noParse: [],
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            },
-            {
-                test: /\.scss$/,
-                include: /\/app\/assets/,
-                loader: 'style!css!sass'
+  target: 'web',
+
+  entry:  {
+    app: path.join(srcPath, 'app.jsx')
+  },
+
+  output: {
+    filename: "[name].js",
+    publicPath: "/",
+    path:path.resolve(__dirname, jsPath, 'build')
+  },
+
+  resolve: {
+    extensions: [".js", ".json", ".css"]
+  },
+
+  devtool: "eval-source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ],
+          plugins: [
+            "transform-node-env-inline"
+          ],
+          env: {
+            development: {
+              plugins: ["react-hot-loader/babel"]
             }
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]--[local]--[hash:base64:8]"
+            }
+          },
+          "postcss-loader" // has separate config, see postcss.config.js nearby
         ]
-    },
-    plugins: [
-        //new webpack.optimize.CommonsChunkPlugin('common', 'common.js'),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            output: { comments: false }
-        }),
-        new webpack.NoErrorsPlugin()
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["es2015", { modules: false }],
+            "stage-2",
+            "react"
+          ]
+        }
+      },
+      {
+        test: /\.scss$/,
+        include: /\/app\/assets/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
     ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      debug: true
+    }),
+  ],
 };
 
-module.exports = config;
+module.exports = settings;
